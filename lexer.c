2789654002673f057f351b12be7c47e41217460d
Isak Horvath <isakhorvath@gmail.com>
2022-12-22 17:22:09 +0100

    NEW: Rework directory structure

--- lexer.c
+++ lexer.c
@@ -152,6 +152,26 @@ void print_symbol_table(Symbol *symbol_table, int num_symbols) {
   }
 }
 
+void write_symbol_table(Symbol *symbol_table, int num_symbols) {
+  FILE *fp = fopen("output/lexer-symtab-out.txt", "w");
+  if (fp == NULL) {
+    perror("Error opening file");
+    return;
+  }
+
+  fprintf(fp, "=============================\n");
+  fprintf(fp, "Symbol Table\n");
+  fprintf(fp, "=============================\n");
+  fprintf(fp, "name        type        value\n");
+  fprintf(fp, "-----------------------------\n");
+  for (int i = 0; i < num_symbols; i++) {
+    Symbol symbol = symbol_table[i];
+    fprintf(fp, "%-12s%-12s%d\n", symbol.name, token_to_string(symbol.type), symbol.value);
+  }
+
+  fclose(fp);
+}
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     printf("Usage: %s <filename>\n", argv[0]);
@@ -173,13 +193,23 @@ int main(int argc, char **argv) {
 
   fclose(file);
 
+  FILE *fp = fopen("output/lexer-tokens-out.txt", "w");
+  if (fp == NULL) {
+    perror("Error opening file");
+    return 1;
+  }
+
   Token token = get_next_token();
   while (token.type != T_EOF) {
     printf("Token(%-2d, %-2d): type=%-12s ival=%-4d rval=%-12f string=%-12s\n", token.col, token.row, token_to_string(token.type), token.ival, token.rval, token.string);
+    fprintf(fp, "Token(%-2d, %-2d): type=%-12s ival=%-4d rval=%-12f string=%-12s\n", token.col, token.row, token_to_string(token.type), token.ival, token.rval, token.string);
     token = get_next_token();
   }
 
+  fclose(fp);
+
   print_symbol_table(symbol_table, num_symbols);
+  write_symbol_table(symbol_table, num_symbols);
 
   return 0;
 }
